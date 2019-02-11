525d56e598b795be7a95c6707f8eeeb435e0c934
Leah <leah29726@gmail.com>
2019-02-11 16:41:42 -0500

    Changed PersonHandler files

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -12,11 +12,19 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        Person currentPerson;
         // assume there is a `counter`
+        int i= 0;
         // while `counter` is less than length of array
             // begin loop
-
-                // use `counter` to identify the `current Person` in the array
+        while(i < personArray.length) {
+        currentPerson = personArray[i];
+        String stringRepresentation = currentPerson.toString();
+        result += stringRepresentation;
+        i++;
+        } 
+        
+        // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
 
@@ -28,10 +36,15 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+        Person currentPerson;
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int i = 0; i < personArray.length; i++){
+         currentPerson = personArray[i];
+        String stringRepresentation = currentPerson.toString();
+        result += stringRepresentation;
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -46,9 +59,16 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+        ;
         // identify array's type
         // identify array's variable-name
-
+        for(Person currentPerson : personArray){
+        String stringRepresentation = currentPerson.toString();
+        result += stringRepresentation;
+        }
+         
+            
+            
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
